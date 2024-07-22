92975f65c347e6998b73538171a3033313647758
Natthawut Suksai <owen55660@gmail.com>
2024-07-22 16:06:07 +0700

    Adding a Stateful widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,7 @@
-import 'package:adv_basic/start_screen.dart';
+import 'package:adv_basic/quiz.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const Quiz());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Color.fromARGB(255, 78, 13, 151),
-                  Color.fromARGB(255, 107, 15, 168),
-                ],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
-            ),
-            child: const StartScreen()),
-      ),
-    );
-  }
-}
